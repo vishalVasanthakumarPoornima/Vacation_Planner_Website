3554d0c99eb6741dd6e648584f12617f2a8e24c8
Aakkash-Muthukumar <aakkash7153@gmail.com>
2025-01-14 13:22:53 -0800

    I just tested the API calls

--- test.py
+++ test.py
@@ -1,28 +1,33 @@
+from amadeus import Client, ResponseError
+import json
+from amadeus import Client, ResponseError
 import os
 import certifi
 
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
-
-
-
-from amadeus import Client, ResponseError, Location
-
-
-# Initialize the Amadeus client
 amadeus = Client(
-    client_id='BAsFVPgNtKMM9E6v9DahzilctL9xB5Jc',
-    client_secret='d6awkC2fGUjd1Rmw',
-    ssl_verify=False  # Disable SSL verification
+    client_id="BAsFVPgNtKMM9E6v9DahzilctL9xB5Jc",
+    client_secret="d6awkC2fGUjd1Rmw"
 )
 
+print()
+
 try:
-    # Example API call: Fetching locations
-    response = amadeus.reference_data.locations.get(
-        keyword="New York",
-        subType="CITY"
-    )
-    print(response.data)
+    response = amadeus.shopping.flight_offers_search.get(
+        originLocationCode='MAD',
+        destinationLocationCode='ATH',
+        departureDate='2025-11-01',
+        adults = 1,
+        children = 2)
+    result = response.result
+
+    file_path = 'flight_offers_response.json'
+    with open(file_path, 'w') as json_file:
+        json.dump(result, json_file, indent=4)  # indent=4 for better readability
+    
+    print(f"Response saved to {file_path}")
 
 except ResponseError as error:
-    print(f"Error occurred: {error}")
+    print(error)
+
